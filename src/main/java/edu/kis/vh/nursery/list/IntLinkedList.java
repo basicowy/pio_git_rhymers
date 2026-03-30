5439904a8b78733d12cae4b03113b3fe2a6677dd
Kamil Buczkowski 255567 <255567@edu.p.lodz.pl>
2026-03-30 12:36:17 +0200

    14 Usunięcie klasy Node i dodanie odpowiedniej logiki do IntLinkeLis

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -9,5 +9,26 @@ public class IntLinkedList {
     public boolean isEmpty() {
         return last == null;
     }
+    private static class Node {
 
+        private final int value;
+        private Node prev;
+        public Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
 }
